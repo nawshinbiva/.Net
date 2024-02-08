ffc46ad40ee396a19487ad70ffd61782d990a40b
Nawshin Nasir <nawshin22nasir@gmail.com>
2024-02-08 22:57:10 +0600

    First update

---
+++ FormSubmission/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Sign up";
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Contact");
+            }
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Success";
+
+            return View();
+        }
+    }
+    
+}
---
+++ FormSubmission/FormSubmission/Models/Person.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.Models
+{
+    public class Person
+    {
+        private string _id;
+
+        [Required(ErrorMessage = "Please Provide Name")]
+        [Display(Name = "Name")]
+        [RegularExpression(@"^[a-zA-Z\s.-]+$", ErrorMessage = "Name can only contain alphabets, spaces, dots and hyphens.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Provide Username")]
+        [Display(Name = "Username")]
+        [RegularExpression(@"^[^\s]+$", ErrorMessage = "Username can only contain alphabets, numbers and special characters(excluding space).")]
+        [StringLength(maximumLength: 10, ErrorMessage = "Username cannot be longer than 10 characters")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Provide ID")]
+        [Display(Name = "ID")]
+        [RegularExpression(@"^\d{2}-\d{5}-\d$", ErrorMessage = "ID must be in the format X-XXXXX-X. Where X is a digit 0-9")]
+        public string Id 
+        {
+            get { return _id; }
+            set
+            {
+                _id = $"{value}@student.aiub.edu";
+            }
+        }
+        [Required(ErrorMessage = "Please Provide Email")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^\d{2}-\d{5}-\d@student.aiub.edu$", ErrorMessage = "Email must be in \"XX-XXXXX-X@student.aiub.edu\" format. Where X is a digit 0-9")]
+        [Compare("Id", ErrorMessage = "Email must be same for ID")]
+        public string Email { get; set; }
+
+
+    }
+}
